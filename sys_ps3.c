0de54ab962b5d0d1c08be99082c0cdced98a1095
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2025-11-06 13:09:42 -0100

    Add sys_ps3.c

---
+++ sys_ps3.c
@@ -0,0 +1,66 @@
+/*
+ * Include this BEFORE darkplaces.h because it breaks wrapping
+ * _Static_assert. Cloudwalk has no idea how or why so don't ask.
+ */
+//#include <SDL.h>
+
+#include "darkplaces.h"
+
+//#ifdef WIN32
+//#ifdef _MSC_VER
+//#pragma comment(lib, "sdl2.lib")
+//#pragma comment(lib, "sdl2main.lib")
+//#endif
+//#endif
+
+
+u64 sys_sdl_ps3_ticks_start = 0;
+
+// =======================================================================
+// General routines
+// =======================================================================
+
+void Sys_SDL_Shutdown(void)
+{
+	// TODO
+}
+
+// Sys_Error early in startup might screw with automated
+// workflows or something if we show the dialog by default.
+static qbool nocrashdialog = true;
+void _nullcb(int a, void *b) {}
+void Sys_SDL_Dialog(const char *title, const char *string)
+{
+	if(!nocrashdialog)
+		//SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, string, NULL);
+		msgDialogOpen(MSG_DIALOG_ERROR|MSG_DIALOG_BTN_TYPE_OK, string, nullcb, NULL, NULL);
+}
+
+char *Sys_SDL_GetClipboardData (void)
+{
+	return NULL;
+}
+
+void Sys_SDL_Init(void)
+{
+	sys_sdl_ps3_ticks_start = sysGetSystemTime();
+
+	// COMMANDLINEOPTION: sdl: -nocrashdialog disables "Engine Error" crash dialog boxes
+	if(!Sys_CheckParm("-nocrashdialog"))
+		nocrashdialog = false;
+}
+
+qbool sys_supportsdlgetticks = true;
+unsigned int Sys_SDL_GetTicks (void)
+{
+	return (u32)(((sysGetSystemTime() - sys_sdl_ps3_ticks_start)/1000) & 0xFFFFFFFF);
+}
+void Sys_SDL_Delay (unsigned int milliseconds)
+{
+	SDL_Delay(milliseconds);
+}
+
+int main(int argc, char *argv[])
+{
+	return Sys_Main(argc, argv);
+}
